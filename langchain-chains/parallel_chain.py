41b939695243571afc38dae02dc800240d10d9c6
Ashmin <asingh13@lululemon.com>
2026-04-13 19:47:36 +0530

    Updated library

--- langchain-chains/parallel_chain.py
+++ langchain-chains/parallel_chain.py
@@ -3,7 +3,7 @@ from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-# from langchain_classic.schema.runnable import RunnableParallel
+# from langchain_core.runnables import RunnableParallel
 
 load_dotenv()
 
