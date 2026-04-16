174372d3b1b9a620017dca969dbad6efe4393a36
Ashmin <asingh13@lululemon.com>
2026-04-16 10:50:03 +0530

    Updated

--- langchain-vectorDB/query_chroma_db.py
+++ langchain-vectorDB/query_chroma_db.py
@@ -5,16 +5,30 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Initialize the vector store with the existing DB
-vectorstore = Chroma(
+vectorstore1 = Chroma(
     persist_directory="langchain-vectorDB/chroma_db",
     embedding_function=OpenAIEmbeddings(),
     collection_name="sample"
 )
 
 # meta-data filtering
-results = vectorstore.similarity_search_with_score(
-    query="",
-    filter={"team": "Chennai Super Kings"}
+results1 = vectorstore1.similarity_search(
+    query='Who among these are a bowler?',
+    k=2)
+
+for i, doc in enumerate(results1):
+    print(f"\n--- Result {i+1} ---")
+    print(doc.page_content)
+
+# Initialize the vector store with the existing DB
+vectorstore2 = Chroma(
+    persist_directory="langchain-vectorDB/chroma_db",
+    embedding_function=OpenAIEmbeddings(),
+    collection_name="my_collection"
 )
 
-print(results)
+results2 = vectorstore2.similarity_search("What is Chroma used for?", k=2)
+
+for i, doc in enumerate(results2):
+    print(f"\n--- Result {i+1} ---")
+    print(doc.page_content)
